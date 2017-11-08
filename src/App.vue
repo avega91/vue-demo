fd7605189245a9b5090d59837172f799f78949eb
Alan Vega <navm_060@hotmail.com>
2017-11-07 23:29:46 -0600

    Added more info, play selected track

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template lang="pug">
   #app
     dm-header
-    section.section
-      nav.nav.has-shadow
+    dm-notification(v-show="showNotification")
+      p(slot="body") No se encontraron resultados
+    dm-loader(v-show="isLoading")
+    section.section(v-show="!isLoading")
+      nav.nav
         .container
           input.input.is-large(
             type="text",
@@ -15,27 +18,32 @@
             small {{ searchMessage }}
 
       .container.results
-        .columns
-          .column(v-for="t in tracks") {{ t.name }} - {{ t.artist}}
-
-
+        .columns.is-multiline
+          .column.is-one-quarter(v-for="t in tracks") 
+            dm-track(:track="t", @select="setSelectedTrack(t.id)", :class="{'is-active': t.id === selectedTrack}")
     dm-footer
 </template>
 
 <script>
-import trackService from './services/track'
-import DmFooter from './components/layout/Footer.vue'
-import DmHeader from './components/layout/Header.vue'
+import trackService from '@/services/track'
+import DmFooter from '@/components/layout/Footer.vue'
+import DmHeader from '@/components/layout/Header.vue'
+import DmTrack from '@/components/Track.vue'
+import DmLoader from '@/components/Shared/Loader.vue'
+import DmNotification from '@/components/Shared/Notification.vue'
 
 export default {
   name: 'app',
   components: {
-    DmFooter, DmHeader
+    DmFooter, DmHeader, DmTrack, DmLoader, DmNotification
   },
   data () {
     return {
       searchQuery: '',
-      tracks: []
+      tracks: [],
+      isLoading: false,
+      showNotification: false,
+      selectedTrack: ''
     }
   },
   computed: {
@@ -49,9 +57,25 @@ export default {
         return
       }
 
+      this.isLoading = true
+
       trackService.search(this.searchQuery).then(res => {
+        this.showNotification = res.tracks.total === 0
         this.tracks = res.tracks.items
+        this.isLoading = false
       })
+    },
+    setSelectedTrack (id) {
+      this.selectedTrack = id
+    }
+  },
+  watch: {
+    showNotification () {
+      if (this.showNotification) {
+        setTimeout(() => {
+          this.showNotification = false
+        }, 3000)
+      }
     }
   }
 }
@@ -61,5 +85,9 @@ export default {
 @import "./scss/main.scss";
 .results {
     margin-top: 50px;
+  }
+
+  .is-active{
+    border: 3px #23d160 solid;
   }
 </style>
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -12,4 +12,13 @@
             .container.has-text-centered
                 h1.title Demo Music
                 h2.subtitle Canciones que están buenísimas
+                dm-player
 </template>
+
+<script>
+import DmPlayer from '@/components/Player.vue'
+
+export default {
+  components: {DmPlayer}
+}
+</script>
